4319d192d7cf8b5d8593f4d93928e5a5aea94eac
bobbynayan <57172340+bobbynayan@users.noreply.github.com>
2019-11-05 10:01:02 +0530

    Delete nayan.py5.py

--- nayan.py5.py
+++
@@ -1,9 +0,0 @@
-l = ("Enter a string :")
-length = len(l)
-for i in range(length) :
-    print("At indexes" , i,"and",(i-length), "element :",l[i])
-    
-print('length is: ',length)
-
-for i in l[::-1]:
-    print(i)
